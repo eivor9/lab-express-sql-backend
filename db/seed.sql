3fb03844103e73a9644fc2f0570cac6555d275cf
eivor9 <njeremiah@pursuit.org>
2024-07-16 23:47:03 -0400

    Update sample data

--- db/seed.sql
+++ db/seed.sql
@@ -2,7 +2,7 @@
 \c songs_dev;
 
 INSERT INTO songs (name, artist, album, time, is_favorite) VALUES
-('Not Like Us', 'Kendrick Lamar', 'Not Like Us - Single', '4 minutes', true),
-('Lies Lies Lies', 'Morgan Wallen', 'Lies Lies Lies - Single', '3 minutes', true),
-('A Bar Song (Tipsy)', 'Shaboozey', 'Where I''ve Been, Isn''t Where I''m Going', '2 minutes', true)
+('Not Like Us', 'Kendrick Lamar', 'Not Like Us - Single', '4:34', true),
+('Lies Lies Lies', 'Morgan Wallen', 'Lies Lies Lies - Single', '3:18', false),
+('A Bar Song (Tipsy)', 'Shaboozey', 'Where I''ve Been, Isn''t Where I''m Going', '2:51', false)
 ;
